e5859d20583731fd047896b7c8a80bf07770d73c
Mike Fierro <mfierro31@yahoo.com>
2023-10-27 16:49:22 -0700

    created database tables for polls - question and choice

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+class Choice(models.Model):
+    """ 
+    Django automatically adds a primary key field for you in your models if you don't specify one yourself,
+    so, here, ForeignKey will reference a unique primary key, we just don't see it here in the code, but it will be
+    created once these tables are created in the database.  The default primary key data type is located in the bottom
+    of mysite/settings.py
+    """
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
