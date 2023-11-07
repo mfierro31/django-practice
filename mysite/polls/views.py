e9d61e1069ff1134bc4694f368b7af07b8d42e73
Mike Fierro <mfierro31@yahoo.com>
2023-11-06 20:41:05 -0800

    registered models in admin, added more views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # The "-" before pub_date is a shortcut for saying "in descending order".  "pub_date" by itself would imply ascending order
+    # [:5] is list slicing syntax that tells us take the first 5 results only (or, up to right before index of 5)
+    latest_questions_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_questions_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking a question # %s." % question_id)
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question # %s." % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question # %s." % question_id)
