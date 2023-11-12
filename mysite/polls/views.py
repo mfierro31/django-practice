2ae4c9117879d06e719c5f0764b8d31cd908134f
Mike Fierro <mfierro31@yahoo.com>
2023-11-11 22:47:03 -0800

    added detail and index templates for polls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,26 @@
 from django.http import HttpResponse
 from .models import Question
-
+from .models import Choice
+from django.shortcuts import render
 
 def index(request):
     # The "-" before pub_date is a shortcut for saying "in descending order".  "pub_date" by itself would imply ascending order
     # [:5] is list slicing syntax that tells us take the first 5 results only (or, up to right before index of 5)
     latest_questions_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_questions_list])
-    return HttpResponse(output)
+    context = {
+        "latest_questions_list": latest_questions_list
+    }
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking a question # %s." % question_id)
+    q = Question.objects.get(pk=question_id)
+    choices = Choice.objects.filter(question__id=question_id)
+    print(choices)
+    context = {
+        "question": q,
+        "choices": choices
+    }
+    return render(request, "polls/detail.html", context)
 
 def results(request, question_id):
     return HttpResponse("You're looking at the results of question # %s." % question_id)
