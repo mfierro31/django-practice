d743e60f1f8b9a884cc2f0794f7e71a81dd91303
Mike Fierro <mfierro31@yahoo.com>
2024-02-15 13:41:41 -0800

    created first POST route

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# Needed when you switch DEBUG to False
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
